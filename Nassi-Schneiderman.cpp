a2bd799b3c5aabed02c32e29e8fac9f902fcb3ba
Negoita Mihai <mihainegoita9@gmail.com>
2022-11-25 17:14:41 +0200

    Am modificat textul butonului principal, am adaugat scroll stanga dreapta, si am modificat umpic incadrarea textului sa functioneze si pentru alte marimi de text :)

--- Nassi-Schneiderman.cpp
+++ Nassi-Schneiderman.cpp
@@ -8,15 +8,14 @@
 using namespace std;
 
 char Path[300];
-int maxH = getmaxheight() / 1.25, maxW = getmaxwidth() / 1.25;
+int maxH = getmaxheight() / 1.15, maxW = getmaxwidth() / 1.15;
 
 void afisare() 
 {
-    settextstyle(DEFAULT_FONT, 0, 140);
     FILE* fptr = fopen(Path, "r");
     cleardevice();
     int nrRanduri = 0, maxRand = 0;
-    char rand[300], maxRandSir[300];
+    char rand[300], maxRandSir[300],auxText[]="I";
     while (fgets(rand, sizeof(rand), fptr)) {
         nrRanduri++;
         if (strlen(rand) > maxRand) {
@@ -24,20 +23,27 @@ void afisare()
             strcpy(maxRandSir, rand);
         }
     }
-    int page = 0, ypoz = 0;
+    int page = 0, ypoz = 0, xpoz = 0;
+    int maxWidthRow = textwidth(maxRandSir);
+    int textHeight = textheight(auxText);
     while (1) {
         setvisualpage(page);
         setactivepage(1 - page);
         cleardevice();
         int r = 1;
         if (GetAsyncKeyState(VK_UP))
-            ypoz-=5;
-        if (GetAsyncKeyState(VK_DOWN))
             ypoz+=5;
+        if (GetAsyncKeyState(VK_DOWN))
+            ypoz-=5;
+        if (GetAsyncKeyState(VK_LEFT))
+            xpoz += 5;
+        if (GetAsyncKeyState(VK_RIGHT))
+            xpoz -= 5;
         rewind(fptr);
-        rectangle(maxW/2-(textwidth(maxRandSir)/2)-maxW*0.01, maxH*0.1+ypoz, maxW / 2 + (textwidth(maxRandSir) / 2) + maxW * 0.01, maxH*0.1+(25*(nrRanduri+1))+ypoz);
+        rectangle(maxW/2-(maxWidthRow /2)-maxW*0.01 + xpoz, maxH*0.1+ypoz,
+                  maxW / 2 + (maxWidthRow / 2) + maxW * 0.01 + xpoz, maxH*0.1+(textHeight*(nrRanduri+1))+ypoz);
         while (fgets(rand, sizeof(rand), fptr)) {
-            outtextxy(maxW / 2 - (textwidth(maxRandSir) / 2), maxH*0.1+(25*r)+ypoz, rand);
+            outtextxy(maxW / 2 - (maxWidthRow / 2) + xpoz, maxH*0.08+ (textHeight * r) + ypoz, rand);
             r++;
         }
         page = 1 - page;
@@ -73,21 +79,23 @@ void alegeFisier()
 void start() 
 {
     int x = 0, y = 0;
-    char word[20] = "Apasa";
+    char word[20] = "Alege fisier text"; // am schimbat textul aici
     char word2[20] = "Iesire";
     rectangle(maxW / 2 - 0.1 * maxW, maxH / 2, maxW / 2 + 0.1 * maxW, maxH / 2 + 0.1 * maxH);
     rectangle(maxW / 2 - 0.1 * maxW, maxH / 2 + 0.2 * maxH, maxW / 2 + 0.1 * maxW, maxH / 2 + 0.3 * maxH);
-    settextstyle(DEFAULT_FONT, 0, 140);
-    outtextxy(maxW / 2 - 0.025 * maxW, maxH / 2 + 0.03 * maxH, word);
-    outtextxy(maxW / 2 - 0.025 * maxW, maxH / 2 + 0.23 * maxH, word2);
+    //settextstyle(DEFAULT_FONT, 0, 0);
+    outtextxy(maxW / 2 - 0.045 * maxW, maxH / 2 + 0.03 * maxH, word);
+    outtextxy(maxW / 2 - 0.02 * maxW, maxH / 2 + 0.23 * maxH, word2);
     while (1) 
     {
         getmouseclick(WM_LBUTTONUP, x, y);
-        if (x >= (maxW / 2 - 0.1 * maxW + 1) && x <= (maxW / 2 + 0.1 * maxW - 1) && y >= (maxH / 2 + 1) && y <= (maxH / 2 + 0.1 * maxH - 1)) 
+        if (x >= (maxW / 2 - 0.1 * maxW + 1) && x <= (maxW / 2 + 0.1 * maxW - 1) 
+                  && y >= (maxH / 2 + 1) && y <= (maxH / 2 + 0.1 * maxH - 1)) 
         {
             alegeFisier();
         }
-        else if (x >= (maxW / 2 - 0.1 * maxW + 1) && x <= (maxW / 2 + 0.1 * maxW - 1) && y >= (maxH / 2 + 0.2 * maxH + 1) && y <= (maxH / 2 + 0.3 * maxH - 1)) 
+        else if (x >= (maxW / 2 - 0.1 * maxW + 1) && x <= (maxW / 2 + 0.1 * maxW - 1) 
+                 && y >= (maxH / 2 + 0.2 * maxH + 1) && y <= (maxH / 2 + 0.3 * maxH - 1)) 
         {
             exit(1);
         }
@@ -96,7 +104,7 @@ void start()
 
 int main()
 {
-    initwindow(getmaxwidth() / 1.25, getmaxheight() / 1.25, "Generator de Diagrame Nassi-Schneiderman");
+    initwindow(maxW, maxH, "Generator de Diagrame Nassi-Schneiderman");
     start();
     getch();
     closegraph();
